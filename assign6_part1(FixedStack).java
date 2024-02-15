2dc7671c8391e258bc0649b2220b288d81ab2f05
Hasitha Reddy <140681337+hasithaa02@users.noreply.github.com>
2024-02-15 14:20:45 +0530

    Update assign6_part1(FixedStack).java

--- assign6_part1(FixedStack).java
+++ assign6_part1(FixedStack).java
@@ -1,151 +1,45 @@
-package Part1a;
+package assignment6;
 
-public interface Interface_STK {
-	public void push(int element);
-	public int pop();
-	public void displayStack();
-	public boolean isOverflow();
-	public boolean isUnderflow();
-
-}
-
-package Part1a;
-
-public class Fixed_stk implements Interface_STK{
+public class FixedStack implements IntStack{
 	
-	private int stack[]=new int[5];
-	private int top=-1;
+	private int stack[];
+	private int top;
 	
-	
-
-	public Fixed_stk(int[] s, int top) {
-		super();
-		this.stack = s;
-		this.top = top;
+	public FixedStack(int len) {
+		stack = new int[len];
+		top = -1;
 	}
-
+	
 	@Override
-	public void push(int element) {
+	public void push(int data) {
 		// TODO Auto-generated method stub
-		if (isOverflow())
-		{
-			System.out.println("Stack is full");
+		if(top == stack.length - 1) {
+			System.out.println("   Stack Overflow");
+		}else {
+			stack[++top] = data;
+			System.out.println("   Data Added!");
 		}
-		else
-		{
-			top=top+1;
-			stack[top]=element;
-		}
-		
 	}
 
 	@Override
 	public int pop() {
 		// TODO Auto-generated method stub
-		int element;
-		if(isUnderflow())
-		{
-			System.out.println("Stack is empty");
-			return -1;
+		if(top < 0) {
+			System.out.println("   Stack Underflow");
+			return 0;
+		}else {
+			return stack[top--];
 		}
-		else
-		{
-			element=stack[top];
-			top=top-1;
-		}
-		
-		
-		return element;
 	}
 
 	@Override
-	public void displayStack() {
+	public boolean isEmpty() {
 		// TODO Auto-generated method stub
-		System.out.println("Stack elements are : ");
-		
-		if(isUnderflow())
-		{
-			System.out.println("No elements");
-		}
-		else
-		{
-			for(int i=0;i<=top;i++)
-			{
-				System.out.println(stack[i]+"\t");
-			}
-		}
-		
-		
-	}
-
-	@Override
-	public boolean isOverflow() {
-		// TODO Auto-generated method stub
-		if (top==(stack.length))
+		if(top < 0) {
 			return true;
-		else
+		}else {
 			return false;
-	
+		}
 	}
 
-	@Override
-	public boolean isUnderflow() {
-		// TODO Auto-generated method stub
-		if (top==-1)
-			return true;
-		else 
-			return false;
-	}
-
-	
-	public int[] getStack() {
-		return stack;
-	}
-
-	public void setStack(int stack[]) {
-		this.stack = stack;
-	}
-
-	public int getTop() {
-		return top;
-	}
-
-	public void setTop(int top) {
-		this.top = top;
-	}
-
-}
-
-package Part1a;
-
-public class Fixed_stk_Demo {
-
-    public static void main(String[] args) {
-        // Create a Fixed_stk object
-        Fixed_stk stack = new Fixed_stk(new int[5], -1);
-
-        // Push elements onto the stack
-        stack.push(10);
-        stack.push(20);
-        stack.push(30);
-
-        // Display the stack
-        stack.displayStack();
-
-        // Pop elements from the stack
-        int poppedElement1 = stack.pop();
-        System.out.println("Popped elements " + poppedElement1 );
-        stack.displayStack();
-        
-        int poppedElement2 = stack.pop();
-        System.out.println("Popped element: " + poppedElement2);
-        stack.displayStack();
-
-        int poppedElement3 = stack.pop();
-        System.out.println("Popped element: " + poppedElement3);
-        stack.displayStack();
-        
-
-        
-    }
 }
